ae2ecaa29e61e670b44273b0e64186974f891a54
shourya mishra <shourya.desk@gmail.com>
2025-03-13 17:40:55 +0530

    ui fix

--- src/ui/BlueButton.jsx
+++ src/ui/BlueButton.jsx
@@ -9,7 +9,7 @@ const BlueButton = ({
     <button
       onClick={onClickHandler}
       disabled={disabled}
-      className='px-4 py-2 disabled:bg-[#E1E3EA] disabled:border-[#E1E3EA] disabled:text-white hover:bg-transparent hover:text-accent-blue border border-accent-blue text-white text-nowrap bg-accent-blue rounded-lg font-medium leading-[1.25rem] text-sm transition-all'
+      className='px-4 py-2 disabled:bg-[#E1E3EA] disabled:border-[#E1E3EA] disabled:text-white hover:bg-transparent hover:text-accent-blue border border-accent-blue text-white text-nowrap bg-accent-blue rounded-md font-medium leading-[1.25rem] text-sm transition-all'
     >
       {text}
     </button>
